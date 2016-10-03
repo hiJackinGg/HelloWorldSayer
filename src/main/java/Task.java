ac91464864a985796bb32e853df37b820ccb7744
Denys Kuznetsov <kuzdenval@gmail.com>
2016-10-03 23:37:07 +0300

    Добавлен плагин для прогона тестов, упрощен код приложения, удалены лишние тесты.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,29 +8,6 @@ public class Task {
 
     private static final Logger log = Logger.getLogger(Task.class);
 
-    /**
-     *
-     * @param res
-     * @param key
-     * @param locale
-     * @return appropriate message
-     */
-    public static String getMessage(final String res, final String key, final Locale locale){
-        log.info("Trying to get needed string from resource bundle...");
-        log.info("Basename: " + res + ". Key word: " + key + ". Locale: " + locale);
-        ResourceBundle bundle = ResourceBundle.getBundle(res, locale);
-        return bundle.getString(key);
-    }
-
-    /**
-     * util to define the key word in resource bundle that corresponds current day period
-     * @param period
-     * @return correct key word stored in resource bundle
-     */
-    public static String convertToMessageKey(DayPeriod period){
-        return period.toString().toLowerCase();
-    }
-
     public static void sayHello(){
 
         //getting user time now
@@ -38,27 +15,74 @@ public class Task {
 
         log.info("Current local time: " + localTime);
 
-        //getting user locale now
+        //getting user local hour now
         final int currentHour = LocalTime.now().getHour();
 
         log.info("Hour: " + currentHour);
 
         //find out the day period now
-        DayPeriod dayPeriod = DayUtils.getDayPeriodByHour(currentHour);
+        String dayPeriod = getDayPeriodByHour(currentHour);
 
         if (dayPeriod == null) {
             log.error("DayPeriod is null !");
             return;
         }else{
-            log.info("Current day period now: " + dayPeriod + "("+dayPeriod.timePeriod()+")");
+            log.info("Current day period now: " + dayPeriod);
         }
 
         //getting appropriate message from resource bundle
-        System.out.println(getMessage("msg", convertToMessageKey(dayPeriod), Locale.getDefault()));
+        log.info("Trying to get needed string from resource bundle...");
+        log.info("Basename: msg. Key word: " + dayPeriod + ". Locale: " + Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("msg", Locale.getDefault());
+
+        System.out.println(bundle.getString(dayPeriod));
+    }
+
+    /**
+     * Defines day period by hour.
+     * @param hour hour of day. It should be [0; 23].
+     * @return defined day period. If day period is not defined, returns null.
+     */
+    public static String getDayPeriodByHour(int hour){
+
+//        if (hour < 0 || hour >= 24)
+//            throw new IllegalArgumentException("Hour: " + hour + "must be in interval [0, 23]");
+
+        if (hour >= 6 && hour < 9){
+
+            log.info(hour + " hours." + "It's morning");
+
+            return "morning";
+        }
+
+        else if (hour >= 9 && hour < 19){
+
+            log.info(hour + " hours." + "It's day");
+
+            return "day";
+        }
+
+        else if (hour >= 19 && hour < 23){
+
+            log.info(hour + " hours." + "It's evening");
+
+            return "evening";
+        }
+
+        else if ((hour >= 23 && hour < 24) ||
+                hour < 6 && hour >= 0){
+
+            log.info(hour + " hours." + "It's night");
+
+            return "night";
+        }
+
+        log.warn(hour + ". Undefined day period");
+
+        return null;
     }
 
     public static void main(String[] args){
-
-            sayHello();
+        sayHello();
     }
 }
--- src/test/java/TaskTests.java
+++ src/test/java/TaskTests.java
@@ -1,35 +1,28 @@
 
 import org.junit.Test;
-import java.util.Locale;
-import java.util.MissingResourceException;
+
 
 import static org.junit.Assert.assertEquals;
 
 
 public class TaskTests {
 
-    @Test(expected=MissingResourceException.class)
-    public void getMessageTest1(){
-        Task.getMessage("wrong_basename", "morning", new Locale("en"));
-
-    }
-
-    @Test(expected=MissingResourceException.class)
-    public void getMessageTest2(){
-        Task.getMessage("msg", "wrong_word", new Locale("en"));
-    }
 
     @Test
-    public void getMessageFromNonexistentLocaleTest3(){
-        Task.getMessage("msg", "morning", new Locale("ua"));
-    }
+    public void getDayPeriodByHourTest(){
 
-    @Test
-    public void convertToMessageKeyTest(){
-        assertEquals("morning", Task.convertToMessageKey(DayPeriod.MORNING));
-        assertEquals("day", Task.convertToMessageKey(DayPeriod.DAY));
-        assertEquals("evening", Task.convertToMessageKey(DayPeriod.EVENING));
-        assertEquals("night", Task.convertToMessageKey(DayPeriod.NIGHT));
+        assertEquals("night", Task.getDayPeriodByHour(0));
+        assertEquals("night", Task.getDayPeriodByHour(23));
+
+        assertEquals("morning", Task.getDayPeriodByHour(6));
+        assertEquals("day", Task.getDayPeriodByHour(9));
+        assertEquals("evening", Task.getDayPeriodByHour(19));
+        assertEquals("night", Task.getDayPeriodByHour(23));
+
+        assertEquals(null, Task.getDayPeriodByHour(24));
+
+        assertEquals(null, Task.getDayPeriodByHour(-23));
+        assertEquals(null, Task.getDayPeriodByHour(1000));
     }
 
     @Test
